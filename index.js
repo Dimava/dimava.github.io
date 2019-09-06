4f4e3d222ec0f949dadee20b8f8adc196b4d1421
Dimava <dimava2@ya.ru>
2019-09-06 23:00:41 +0300

    whatever

--- index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    document.getElementById("shoutout"+Math.floor(Math.random()*5+1)).style.display = "flex";
-};
-
-function disableShoutoutAnim() {
-    let text = document.getElementById("shoutouttext")
-    text.style.animation = "none"
-    text.style.opacity = "0"
-}
