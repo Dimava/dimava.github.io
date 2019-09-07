799ee07840f21d466cb7c840898f73e8c136e46c
Dimava <dimava2@ya.ru>
2019-09-07 10:52:54 +0300

    cache

--- helpmod/js/js.js
+++ helpmod/js/js.js
@@ -1,19 +1,36 @@
+
+    luaName = 'seablock';
+
 async function start_loading_json() {
 
     let b = document.querySelector('button');
     b.disabled = true;
     b.innerText = 'Loading...';
 
-    luaName = 'seablock';
+    __init__;
 
-    json_wait = fetch(`./data_raw_${luaName}_processed.json?v=001`).then(r=>r.text());
+    json_wait = fetch.cached(`./data_raw_${luaName}_processed.json?v=001`).then(r=>r.text());
 
     json_data_raw = await json_wait;
 
-    __init__;
 
     [raw,entry_list] = just_load_json();
 
     start_inventory_vue();
 
 }
+
+void async function() {
+    let url = `./data_raw_${luaName}_processed.json?v=001`;
+
+    let cache = await caches.open('fetch');
+
+    let response = await cache.match(url);
+
+    if (response) {
+        document.querySelector('span').innerHTML += `
+            <br> This file is already cached so it wont use much time.
+        `;
+    }
+
+}();
