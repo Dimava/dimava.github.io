5a8dca99ccde54758e0e4ffd675fa6a843fc2a18
Dimava <dimava2@ya.ru>
2020-08-08 17:25:53 +0300

    _push

--- _push.bat
+++ _push.bat
@@ -1,13 +1,22 @@
 
+cd "../shapez.io/gulp/"
+
+cmd /c "yarn gulp build.dev"
+
+cmd /c "yarn gulp js.dev"
+
+echo "build complete"
+
+cd "../../dimava.github.io/"
+
 git pull --ff-only
 
 robocopy "../shapez.io/src/" "./" "index.html"
 
-robocopy "../shapez.io/build" "./shapez/modZ" /S
+robocopy "../shapez.io/build/" "./shapez/modZ/" /S
 
 git add -A
 
 git commit -m "_push"
 
 git push
-
