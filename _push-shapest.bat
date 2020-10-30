fa8e30a36a87b249ee65562760b37c20d004d589
Dimava <dimava2@ya.ru>
2020-10-30 20:43:09 +0300

    _push-shapest

---
+++ _push-shapest.bat
@@ -0,0 +1,20 @@
+
+cd "../shapez.io/gulp/"
+
+cmd /c "yarn gulp build.prod"
+
+echo "build complete"
+
+cd "../../dimava.github.io/"
+
+git pull --ff-only
+
+robocopy "../shapez.io/src/" "./" "index.html"
+
+robocopy "../shapez.io/build/" "./shapez/shapest" /S
+
+git add -A
+
+git commit -m "_push-shapest"
+
+git push
