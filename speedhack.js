7ddb1473b55d681f4c7fe508fd4d3981ef966836
Dimava <dimava2@ya.ru>
2021-11-10 15:06:24 +0300

    Create speedhack.js

---
+++ speedhack.js
@@ -0,0 +1,59 @@
+// I will expland this to "How to speedhack in any web game which uses Date for timing"
+
+// First of all we should save a way to get the correct time, the current state, and add some varuables for future time modification
+Date.prototype._getTime = Date.prototype._getTime || Date.prototype.getTime;
+Date._now = Date._now || Date.now;
+Date.timeAtStart = Date.now();
+Date.realtimeAtStart = Date._now();
+Date.deltaTime = 0;
+Date.timeMulti = 1;
+// Then we should override all the methods which converts Date into a number. Don't forget rounding, some games will error if the time is not an integer as it should be.
+Date.prototype.getTime = function() {
+    let realtimeSince = this._getTime() - Date.realtimeAtStart;
+    let timeSince = timeSince * Date.timeMulti + Date.deltaTime;
+    return Math.round(Date.timeAtStart + timeSince);
+}
+Date.valueOf = function() { return this.getTime(); }
+Date.now = function() { return new Date().getTime(); }
+// Then lets make functions to modify the time: time jump and speedhack. To make a clear way to see speedhack time multiplier, we'll push it into site url
+Date.timeskip = function (seconds = 10) {
+    Date.deltaTime += seconds * 1000;
+}
+Date.speedhack = function (speed = 2) {
+    Date.timeAtStart = Date.now();
+    Date.realtimeAtStart = Date._now();
+    Date.deltaTime = 0;
+    location.hash = 'x' + speed;
+    Date.timeMulti = speed;
+}
+// To be able to use all of this outside the console, lets make some keybindings
+if (!Date._hasKeydownListener)
+    addEventListener('keydown', event => {
+        let fn = Date.kds[event.code] || Date.kds[event.key];
+        if (fn) {
+            fn(event);
+            event.stopPropagation();
+        }
+}, true);
+Date._hasKeydownListener= true;
+Date.timeMultis = [1, 2, 5, 10, 20, 60];
+Date.kds = {
+    // number keys skip time
+    1: () => skip(10),
+    2: () => skip(30),
+    3: () => skip(1 * 60), // one minute
+    4: () => skip(5 * 60),
+    5: () => skip(15 * 60),
+    6: () => skip(60 * 60), // one hour
+    // bracket keys do speedhack
+    "[": () => {
+        let speed = Date.timeMultis[Date.timeMultis.indexOf(Date.timeMulti)-1];
+        if (speed) speedhack(speed);
+    },
+    "]": () => {
+        let speed = Date.timeMultis[Date.timeMultis.indexOf(Date.timeMulti)+1];
+        if (speed) speedhack(speed);
+    },
+}
+// - number keys skip time, . You will want to change those for the games with colliding keybinds.
+// Note, however, that timejumps work properly only in games with inplemented offline time.
