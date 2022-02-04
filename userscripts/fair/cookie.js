7a409310f4e1b2abc36faea6c93d233a7215de77
dimava <dimava2@ya.ru>
2022-02-04 21:34:23 +0300

    up

--- userscripts/fair/cookie.js
+++ userscripts/fair/cookie.js
@@ -1,19 +1,19 @@
 window._getCookie ??= getCookie;
 getCookie = cname => {
-    if (cname == '_uuid') {
-    	let hash = location.hash.slice(1)
-    	if (hash.length == 36) {
-			return hash;
-		}
-    	if (hash.startsWith('acc') && localStorage[hash]) return localStorage[hash];
-    }
-    return _getCookie(cname);
+	if (cname == '_uuid') {
+		let hash = location.hash.slice(1)
+		if (hash.length == 36) return hash;
+		if (hash.startsWith('acc') && localStorage[hash]) return localStorage[hash];
+	}
+	return _getCookie(cname);
 }
 setInterval(() => {
-	localStorage['acc' + identityData.uuid] = identityData.accountId;
-	localStorage['acc' + identityData.accountId] = identityData.uuid;
-	if (location.hash == '#' + identityData.uuid) {
-		location.hash = 'acc' + localStorage['acc' + hash];
+	if (identityData.accountId) {
+		localStorage['acc' + identityData.uuid] = identityData.accountId;
+		localStorage['acc' + identityData.accountId] = identityData.uuid;
+		if (location.hash == '#' + identityData.uuid) {
+			location.hash = 'acc' + localStorage['acc' + hash];
+		}
 	}
 	document.title = `#${ladderData.yourRanker.rank} ${ladderData.yourRanker.username || 'disconnected'} - Fair Game`;
 }, 10e3)
